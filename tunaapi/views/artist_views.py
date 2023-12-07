a2a467050a6de8e2256e84b91b2ec5e36fb0d3f1
Thomas Rice <ricethomasj@gmail.com>
2023-12-06 20:55:35 -0600

    Added full CRUD on artists and genres

--- tunaapi/views/artist_views.py
+++ tunaapi/views/artist_views.py
@@ -24,6 +24,45 @@ class ArtistView(ViewSet):
     """Handle GET requests for all artists
     
     Returns -> Response -- JSON serialized list of artists"""
+    
+    try:
+      artists = Artist.objects.all()
+      serializer = ArtistSerializer(artists, many=True)
+      return Response(serializer.data)
+    except Artist.DoesNotExist as ex:
+      return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+    
+  def create(self, request):
+    """Handle POST operations for artists
+    
+    Returns -> JSON serialized artist instance"""
+    
+    artist = Artist.objects.create(
+      name = request.data['name'],
+      age = request.data['age'],
+      bio = request.data['bio'],
+    )
+    
+    serializer = ArtistSerializer(artist)
+    return Response(serializer.data)
+  
+  def update(self, request, pk):
+    """Handles PUT reqeust for an artist
+    
+    Returns -> Empty body with 204 status code"""
+    
+    artist = Artist.objects.get(pk=pk)
+    artist.name = request.data['name']
+    artist.age = request.data['age']
+    artist.bio = request.data['bio']
+    
+    artist.save()
+    return Response(None, status=status.HTTP_204_NO_CONTENT)
+  
+  def destroy(self, request, pk):
+    artist = Artist.objects.get(pk=pk)
+    artist.delete()
+    return Response(None, status=status.HTTP_204_NO_CONTENT)
 
 class ArtistSerializer(serializers.ModelSerializer):
   """JSON serializer for artists"""
--- tunapiano/urls.py
+++ tunapiano/urls.py
@@ -17,7 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
+from tunaapi.views import GenreView, ArtistView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'artists', ArtistView, 'artist')
+router.register(r'genres', GenreView, 'genre')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
